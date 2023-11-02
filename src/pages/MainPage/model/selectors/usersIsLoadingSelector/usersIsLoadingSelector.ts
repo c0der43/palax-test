117916d278d2481199f9d4d32d700315185c07d8
c0der20 <newdeveloper43@gmail.com>
2023-11-02 14:22:55 +0300

    visible loading

---
+++ src/pages/MainPage/model/selectors/usersIsLoadingSelector/usersIsLoadingSelector.ts
@@ -0,0 +1,3 @@
+import {StateSchema} from "@/app/providers/StoreProvider/config/StateSchema.ts";
+
+export const usersIsLoadingSelector = (state: StateSchema) => state.mainPage?.users.isLoading ?? false;
